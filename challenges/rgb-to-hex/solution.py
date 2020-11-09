21e07551143134ff7d834bb3c2a0e9b7eb1ed8e2
AnonymouX47 <feyidab01@gmail.com>
2020-11-09 02:02:49 +0100

    Added rgb-to-hex python solution

---
+++ challenges/rgb-to-hex/solution.py
@@ -0,0 +1,7 @@
+def rgb(*args):
+    return "#" + ("%02X" * 3) % (*[min(255, max(0, x)) for x in args],)
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        print(rgb(*[int(x) for x in input().split()]))
