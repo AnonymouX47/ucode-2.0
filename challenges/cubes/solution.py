85531af0661280b5dd8dae4f42145655896c31c0
AnonymouX47 <feyidab01@gmail.com>
2020-11-10 08:02:49 +0100

    Fixed Solution to solve all possible edge cases known.

--- challenges/cubes/solution.py
+++ challenges/cubes/solution.py
@@ -1,7 +1,13 @@
-from math import ceil
+from math import ceil, floor
 
 def solve(a, b):
-    return int((b ** (1/3) + 0.000001) - ceil(a ** (1/3))) + 1
+    lower = round(a ** (1/3), 8)
+    upper = round(b ** (1/3), 8)
+
+    if ceil(lower) == floor(upper):
+        return int(lower.is_integer() or upper.is_integer())
+    else:
+        return floor(upper) - ceil(lower) + 1
 
 if __name__ == "__main__":
     for _ in range(int(input())):
