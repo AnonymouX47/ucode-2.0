3c4997caae1e7dcf5a0152ed92aabad4d6f11be5
AnonymouX47 <feyidab01@gmail.com>
2020-11-11 17:38:07 +0100

    Completed input, approach selection and first approch.

--- challenges/queens/solution.py
+++ challenges/queens/solution.py
@@ -1,8 +1,24 @@
 
-def queensAttack(n, k, r_q, c_q, obstacles):
-    if n == 1: return 0
-    if k == 0:
-        pass
+def total_squares(n: int, pieces: list, positions: list):
+    hori_vert = (n-1) * 2
+    dia_r_up = lambda r, c: min(n - c, n - r)
+    dia_r_dn = lambda r, c: min(n - c, r - 1)
+    dia_l_up = lambda r, c: min(c - 1, n - r)
+    dia_l_dn = lambda r, c: min(c - 1, r - 1)
+
+    max_squares = {'q': lambda r, c: (hori_vert
+                                        + dia_r_up(r, c) + dia_r_dn(r, c)
+                                        + dia_l_up(r, c) + dia_l_dn(r, c)),
+                    'b': lambda r, c: (dia_r_up(r, c) + dia_r_dn(r, c)
+                                        + dia_l_up(r, c) + dia_l_dn(r, c)),
+                    'r': lambda r, c: hori_vert
+                    }
+
+    return sum([max_squares[piece](*pos) for piece, pos in zip(pieces, positions)])
+
+
+def solve1(n: int, piece: str, r_p: int, c_p: int, obstacles: set):
+    """ Loop over obstacles and check for nearest to each piece in each direction. """
 
     squares = 0
     # Up
@@ -37,3 +53,33 @@ def queensAttack(n, k, r_q, c_q, obstacles):
     return squares
 
 
+def solve(n: int, pieces: list, positions: list, obstacles, method: int):
+    method = (None, solve1, solve2, solve3)[method]
+
+    return max([method(n, piece, *pos, obstacles) for piece, pos in zip(pieces, positions)])
+
+
+if __name__ == "__main__":
+    n, p, k = map(int, input().split())
+    pieces = input().split()
+    positions = [tuple(input().split()) for _ in range(p)]
+    if n == 1:
+        print(f"0\n{pieces[0]} {' '.join(positions[0])}\n")
+    if total_squares > k:
+        method = 1
+        obstacles = {tuple(map(int, input().split())) for _ in range(k)}
+    elif n <= 20 * 10**3:  # Traverse pieces' paths and check for nearest obstacle using a (1 x N**2) bytearray.
+        method = 2
+        # Array should use <= ~400MB
+        obstacles = bytearray(b'\0') * n**2
+        for _ in range(k):
+            r, c = map(int, input().split())
+            obstacles[n*(r - 1) + c] = 1
+        for r, c in positions:
+            obstacles[n*(r - 1) + c] = 1
+    else:  # Traverse pieces' paths and check for nearest obstacle using membership tests on a set.
+        method = 3
+        obstacles = {tuple(map(int, input().split())) for _ in range(k)}
+
+
+    solve(n, pieces, positions, obstacles, method)
