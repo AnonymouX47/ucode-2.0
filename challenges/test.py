c8b43e32c1fbb223a029806d97abe2d2c7221c74
AnonymouX47 <feyidab01@gmail.com>
2020-11-12 15:37:06 +0100

    Added pseudo command-line commands to control output of test suite

--- challenges/test.py
+++ challenges/test.py
@@ -16,15 +16,17 @@ for filename in sorted(os.listdir(f"{challenge}/testcases/input/"),
     output = evaluate("python testcase.py", text=f"{challenge}\n{filename}")
     results[filename] = eval(output[2])
 
-print(*map("{0[0]}: {0[1]}".format, results.items()), sep='\n')
-print()
+if "print" in sys.argv:
+    print(*map("{0[0]}: {0[1]}".format, results.items()), sep='\n')
+    print()
 
-print("Total Time: %.2f" % sum([res[1][0] for res in results.items()]))
-print("Total Space: %i\n" % sum([res[1][1] for res in results.items()]))
-print("Worst Time: {}\n{}\n".format(*max(results.items(), key=lambda t :t[1][0])))
-print("Worst Space: {}\n{}\n".format(*max(results.items(), key=lambda t :t[1][1])))
+if "profile" in sys.argv:
+    print("Total Time: %.2f" % sum([res[1][0] for res in results.items()]))
+    print("Total Space: %i\n" % sum([res[1][1] for res in results.items()]))
+    print("Worst Time: {0}\n{1[0]}\n".format(*max(results.items(), key=lambda t :t[1][0])))
+    print("Worst Space: {0}\n{1[1]}\n".format(*max(results.items(), key=lambda t :t[1][1])))
 
-if len(sys.argv) > 2:
+if "check" in sys.argv:
     for result, filename in zip(results.values(),
                                 sorted(os.listdir(f"{challenge}/testcases/output/"),
                                         key=lambda s :s[-6:])):
